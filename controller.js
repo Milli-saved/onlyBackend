18f99f7e547ec63c77aff1526157bcd3cff6273f
Milli-saved <million12.tenkir@gmail.com>
2022-11-16 15:57:57 +0300

    All is done.

--- controller.js
+++ controller.js
@@ -1,5 +1,15 @@
 const Person = require("./model");
 
+exports.params = async (req, res, next, reqId) => {
+  const person = await Person.findById(reqId);
+  if (!person) {
+    next(new Error("no one found"));
+  } else {
+    req.person = person;
+    next();
+  }
+};
+
 exports.addPerson = async (req, res, next) => {
   try {
     const person = await Person.create(req.body);
@@ -21,6 +31,58 @@ exports.getAllUsers = async (req, res, next) => {
     res.status(200).json({
       status: "success",
       person,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      error: err,
+    });
+  }
+};
+
+exports.getOnePerson = async (req, res, next) => {
+  try {
+    let person = req.person;
+    res.status(200).json({
+      status: "successful",
+      person,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      error: err,
+    });
+  }
+};
+
+exports.deletePerson = async (req, res, next) => {
+  try {
+    req.person.remove().then(() => {
+      res.status(200).json({
+        status: "successful",
+        deleted: "true",
+      });
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      error: err,
+    });
+  }
+};
+
+exports.updatePerson = async (req, res, next) => {
+  try {
+    let person = req.person;
+    let updatedPerson = await Person.updateOne(
+      { _id: person._id },
+      {
+        $set: req.body,
+      }
+    );
+    res.status(200).json({
+      status: "successful",
+      person: updatedPerson,
     });
   } catch (err) {
     res.status(400).json({
--- route.js
+++ route.js
@@ -3,8 +3,16 @@ const personController = require("./controller");
 
 const router = express.Router();
 
+router.param("id", personController.params);
+
 router
   .post("/addperson", personController.addPerson)
   .get("/getperson", personController.getAllUsers);
+
+router
+  .route("/:id")
+  .delete(personController.deletePerson)
+  .get(personController.getOnePerson)
+  .put(personController.updatePerson);
 
 module.exports = router;
